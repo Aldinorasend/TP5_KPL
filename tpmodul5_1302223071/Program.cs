a507ce6f61b5eda878f16f7239e1bcb96688b501
Aldinorasend <aldinorasend@gmail.com>
2024-03-17 04:17:16 +0700

    menambahkan class HaloGeneric dengan method SapaUser

--- tpmodul5_1302223071/Program.cs
+++ tpmodul5_1302223071/Program.cs
@@ -8,8 +8,23 @@ namespace tpmodul5_1302223071
 {
     class Program
     {
+        class HaloGeneric {
+            
+            public void SapaUser(String nama)
+            {
+                Console.WriteLine("Halo User " + nama);
+            }
+        }
+
         static void Main(string[] args)
         {
+            String nama;
+            Console.WriteLine("Masukkan Nama User : ");
+            nama = Console.ReadLine();
+            HaloGeneric halo = new HaloGeneric();
+            halo.SapaUser(nama);
+            Console.Read();
         }
+
     }
 }
